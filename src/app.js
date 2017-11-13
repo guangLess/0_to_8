7281ad3b71ba19ace7dc61f8d312a533cc330cdf
guangLess <bha.guang@gmail.com>
2017-11-12 19:42:26 -0500

    Ignore? Just kidding

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Root from './Root'
-
-const app = document.getElementById('app')
-ReactDOM.render(Root, app);
--- webpack-middleware.js
+++
@@ -1,8 +0,0 @@
-
-const webpackConfig = require('./webpack.config')
-, {output: {path}} = webpackConfig
-
-module.exports = require('webpack-dev-middleware')(
-require('webpack')(webpackConfig),
-{path}
-)
