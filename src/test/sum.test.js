8d637d7c4ddf2a38758f9f9e0712c497d3ded4d8
guangLess <bha.guang@gmail.com>
2018-01-06 18:41:52 -0500

    clean up in test file

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,35 +1,31 @@
-const sum = require('../sum.js');
-import { shuffle } from '../helper'
+//const sum = require('../sum.js');
+import { shuffle, swapArrayElements } from '../helper'
 
+describe('helper functions', () => {
+    describe('shuffle board', () => {
+        let numArray = [0, 1, 2, 4];
+        let numLength = numArray.lenght;
 
-describe('empty', () => {
-    it('should work', () => {
-        expect(true).toEqual(true);
-    });
-})
+        it('should have the same lenght after shuffled', () => {
+            shuffle(numArray);
+            expect(numArray.lenght).toEqual(numLength);
+        })
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
-
-describe('shuffle board', () => {
-    let numArray = [0, 1, 2, 4];
-    let copy = numArray.slice()
-    console.log(copy)
-    let numLength = numArray.lenght;
-    shuffle(numArray);
-    console.log(numArray)
-
-    it ('should have the same lenght after shuffled', () => {
-        expect(numArray.lenght).toEqual(numLength);
-    })
-    
-    it('should shuffle this array with at least one different ordering element', ()  => {
-        for (let i = 0 ; i < numLength; i++) {
+        it('should shuffle this array with at least one different ordering element', () => {
+            let copy = numArray.slice();
+            for (let i = 0; i < numLength; i++) {
                 const num = numArray[i];
-                console.log(num, i, copy[i])            
-                expect((num !== copy[i]) ).toEqual(true);
-        }
+                expect((num !== copy[i])).toEqual(true);
+            }
+        })
+    });
+
+    describe('swapArray function', () => {
+        it('should swap the position of two element', () => {
+            
+        })
+
     })
+
 });
 
