8400c3bbefa4ce0ec39fbe3f2519891830702dbe
guangLess <bha.guang@gmail.com>
2018-01-07 19:19:02 -0500

    more tests for Redux

--- src/test/react.test.js
+++ src/test/react.test.js
@@ -1,18 +1,34 @@
 
 import store, { CREATE_BOARD, createBoard, getMemoryBoard, memoryStack} from '../redux/reducer'
-import configureMockStore from 'redux-mock-store'
+import configureStore from 'redux-mock-store'
+//import fetchMock from 'fetch-mock'
+const mockStore = configureStore({memoryStack});
 //console.log(store)
 
 describe('Redux Actions', () => {
-    const memories = ['h', 'x', 1, '💪🏼', '🤖']
+    //const memories = ['h', 'x', 1, '💪🏼', '🤖']
+    //let mStore = mockStore(memoryStack);
+    // describe('async actions', () => {
+    //     beforeEach( () => {
+    //         mockStore = mockStore(memoryStack);
+    //     })
+    // })
+
     it('this action should create board as 1_D array', () => {
         const expectedBoard = {
             type: CREATE_BOARD,
-            memories
+            memories: memoryStack
         };
-        expect(createBoard(memories)).toEqual(expectedBoard)
+        expect(createBoard(memoryStack)).toEqual(expectedBoard)
     });
-
+// test connect
+/*
+    it('Should dispatch .....', () => {
+        mStore.dispatch(getMemoryBoard)
+        let newBoard = mockStore.getState();
+        console.log("newBoard", newBoard)
+    })
+*/
 })
 
 describe('store', () => {
@@ -22,5 +38,4 @@ describe('store', () => {
         //console.log(actual, "----", expected)
         expect(actual).toEqual(expected)
     })
-    //it ('should ')
 })
