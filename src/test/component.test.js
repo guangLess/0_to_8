c666721aa51edfe27e79dcccbcf8b9165fd9ef7d
guangLess <bha.guang@gmail.com>
2018-01-17 16:12:45 -0500

    start to test containers

--- src/test/component.test.js
+++ src/test/component.test.js
@@ -1,14 +1,54 @@
 import React, {Component} from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Enzyme, { shallow, configure, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import thunk from 'redux-thunk'
 import Square from '../components/Square.jsx';
+import Game from '../components/Game.jsx';
 
-configure({ adapter: new Adapter() });
 
+import configureMockStore from 'redux-mock-store'
 
+Enzyme.configure({ adapter: new Adapter()});
+function setup() {
+    const props = {
+        mountMemories: jest.fn()
+    }
+    const gameWrapper = mount(<Game {...props} />)
+    return {
+        props,
+        gameWrapper
+    }
+}
+
+describe('components', () => {
+    describe('Game', () => {
+    const middlewares = [thunk]
+    const mockStore = configureMockStore(middlewares);
+    const initialState = {mempries: [1,2,3]}
+    let store, container
+
+    beforeEach( () => {
+        store = mockStore(initialState)
+        container = shallow(<Game store= {store} />)
+
+    })
+
+    it('renders connected <Game /> component', () => {
+        expect(container.length).toEqual(1);
+    })
+    /*it('should render itself with props', () => {
+            const {gameWrapper, props} = setup()
+            expect(props.mountMemories.mock.calls.length).toBe(1)
+            console.log(gameWrapper)
+        })
+    })*/
+    })
+})
+
+/*
 describe('<Square />', () => {
     it('should render <Board /> components', () => {
         const wrapper = shallow(<Square />);
         //expect(wrapper.find(Square)).to.have.length(9)
     })
-})
+*/
